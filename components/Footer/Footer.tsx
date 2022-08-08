b5073a96007e0ccc9458f934451651ddfb645518
amifsud02 <amifsud02@gmail.com>
2022-08-08 23:47:12 +0200

    Updated About Us

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,13 +1,37 @@
 const Footer = () => {
     return (
         <footer>
-            <div className="main-footer"></div>
+            <div className="main-footer">
+                <div>
+                    <div>La Salle</div>
+                    <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</div>
+                    <div>Social Media Icons</div>
+                </div>
+                <div>
+                    <ul>
+                        <li>Home</li>
+                        <li>About Us</li>
+                        <li>Management</li>
+                        <li>Matches</li>
+                        <li>Fixtures</li>
+                        <li>Team</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+                <div>
+                    <div>Contact Us</div>
+                    <div>Email</div>
+                    <div>Mobile</div>
+                    <div>Address</div>
+                </div>
+            </div>
+
             <div className="sub-footer">
                 <div className="sf-content">
                     © 2022 - La Salle Handball Club
                 </div>
                 <div className="sf-content">
-                    ©Designed and Developed by <span>TPLL</span>
+                    Designed and Developed by <span>TPLL</span>
                 </div>
             </div>
         </footer>
---
+++ pages/admin/index.tsx
@@ -0,0 +1,3 @@
+import Head from "next/head";
+import { useEffect, useState } from "react";
+
