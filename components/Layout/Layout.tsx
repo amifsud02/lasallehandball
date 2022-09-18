5019c8e13bb4c1981d1590130526143f3aecce83
amifsud02 <amifsud02@gmail.com>
2022-09-18 22:32:46 +0200

    Added meta tags

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -11,9 +11,29 @@ export default function Layout(props: Props){
         <>
             <Head>
                 <title>{props.title}</title>
-                <meta name="apple-mobile-web-app-capable" content="yes" />
+
+                <meta charSet="UTF-8"></meta>
+                <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+
+                <meta name="title" content={`La Salle Handball Club - ${props.title}`}></meta> 
+                <meta name="description" content="An amateur club with a professional mentality."></meta> 
+
+                <meta property="og:type" content="website"></meta> 
+                <meta property="og:url" content="https://metatags.io/"></meta> 
+                <meta property="og:title" content={`La Salle Handball Club - ${props.title}`}></meta> 
+                <meta property="og:description" content="An amateur club with a professional mentality."></meta> 
+                {/* <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"></meta>  */}
+
+                <meta property="twitter:card" content="summary_large_image"></meta> 
+                <meta property="twitter:url" content="https://metatags.io/"></meta> 
+                <meta property="twitter:title" content={`La Salle Handball Club - ${props.title}`}></meta> 
+                <meta property="twitter:description" content="An amateur club with a professional mentality."></meta> 
+                {/* <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"></meta>     */}
             </Head>
+
             {props.children}
+
             <Footer></Footer>
         </>
     )
