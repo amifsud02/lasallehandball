def272a404a9d006441d5bb2e7221c5045b8a114
amifsud02 <amifsud02@gmail.com>
2022-09-18 22:36:31 +0200

    meta tag httpequiv fix

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -13,7 +13,7 @@ export default function Layout(props: Props){
                 <title>{props.title}</title>
 
                 <meta charSet="UTF-8"></meta>
-                <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge"></meta>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
 
                 <meta name="title" content={`La Salle Handball Club - ${props.title}`}></meta> 
