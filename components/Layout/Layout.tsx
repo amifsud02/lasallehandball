07aca56beacd8a301c20b0602e41f56a584aa422
amifsud02 <amifsud02@gmail.com>
2023-01-31 23:43:17 +0100

    Font

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -36,6 +36,12 @@ export default function Layout(props: Props){
                 <meta property="twitter:description" content="An amateur club with a professional mentality."></meta> 
                 {/* <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"></meta>     */}
 
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap"
+                    rel="stylesheet"
+                />
             </Head>
         
             {props.children}
